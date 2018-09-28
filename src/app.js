76f545a1edcab5b206b78ee0a43dc3d78dce2a8c
Ewerton Batista Lauredo <Ewerton@MacBook-Air-de-Ewerton.local>
2018-09-28 07:36:59 -0300

    Conexao com Banco de Dados e Inclusao

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Conecta ao banco
+mongoose.connect('mongodb://teste:teste@ds117623.mlab.com:17623/nodestr',{ useNewUrlParser: true });
+mongoose.set('useCreateIndex', true)
+
+//Carrega os Models
+const Product = require('./models/product');
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route');
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,8 +1,64 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+exports.get = (req, res, next) => {
+    Product.find({ active: true }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e)
+        });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({ 
+        slug: req.params.slug,
+        active: true 
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e)
+        });
+}
+
+exports.getById = (req, res, next) => {
+    Product.findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e)
+        });
+}
+
+exports.getByTag = (req, res, next) => {
+    Product
+        .find({ 
+            tags: req.params.tag,
+            active: true 
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e)
+        });
+}
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
-};
+    let product = new Product(req.body);
+    product
+        .save()
+        .then(x => {
+            res.status(201).send({ message: 'Produto cadastrado com sucesso!' });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao cadastrar o produto',
+                data: e
+            })
+        });
+}
 
 exports.put = (req, res, next) => {
     const id = req.params.id;
@@ -10,8 +66,8 @@ exports.put = (req, res, next) => {
         id: id,
         item: req.body
     });
-};
+}
 
 exports.delete = (req, res, next) => {
     res.status(200).send(req.body);
-};
+}
