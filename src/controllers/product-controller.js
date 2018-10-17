6a290534763de8f9c2a937203f105183331f2e51
Ewerton Batista Lauredo <Ewerton@MacBook-Air-de-Ewerton.local>
2018-10-17 17:51:54 -0300

    Adicao Repositorios

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,21 +2,34 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const repository = require('../repositories/product-repository');
 
+exports.get = async(req, res, next) => {
+    try {
+        let data = await repository.get();
+        res.status(200).send(data);
+    } catch(e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisicao'
+        });
+    }
+    
+}
+
+/*
 exports.get = (req, res, next) => {
-    Product.find({ active: true }, 'title price slug')
+        repository
+        .get()
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
             res.status(400).send(e)
         });
-}
+}*/
 
 exports.getBySlug = (req, res, next) => {
-    Product.findOne({ 
-        slug: req.params.slug,
-        active: true 
-        }, 'title description price slug tags')
+        repository
+        .getBySlug(req.params.slug)
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
@@ -25,7 +38,7 @@ exports.getBySlug = (req, res, next) => {
 }
 
 exports.getById = (req, res, next) => {
-    Product.findById(req.params.id)
+        repository.getById(req.params.id)
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
@@ -35,10 +48,7 @@ exports.getById = (req, res, next) => {
 
 exports.getByTag = (req, res, next) => {
     Product
-        .find({ 
-            tags: req.params.tag,
-            active: true 
-        }, 'title description price slug tags')
+        repository.getByTag(req.params.tag)
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
@@ -47,9 +57,7 @@ exports.getByTag = (req, res, next) => {
 }
 
 exports.post = (req, res, next) => {
-    let product = new Product(req.body);
-    product
-        .save()
+        repository.create(req.body)
         .then(x => {
             res.status(201).send({ message: 'Produto cadastrado com sucesso!' });
         }).catch(e => {
@@ -61,13 +69,29 @@ exports.post = (req, res, next) => {
 }
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
+    repository.update(req.params.id, req.body)
+    .then(x => {
+        res.status(200).send({
+            message: 'Produto atualizado com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao atualizar o produto',
+            data: e
+        });
+    })
 }
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    repository.delete(req.body.id)
+    .then(x => {
+        res.status(200).send({
+            message: 'Produto removido com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao remover o produto',
+            data: e
+        });
+    });
 }
