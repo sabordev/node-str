ec44f187a3d8896d8137afa9db747f98a1604ee5
Sabor Dev <sabor.dev6@gmail.com>
2018-10-28 18:25:38 -0300

    Project Paused

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const router = express.Router();
 //Conecta ao banco
 const strConnection = 'mongodb://teste:teste@ds117623.mlab.com:17623/nodestr';
 mongoose.connect(strConnection,{ useNewUrlParser: true });
-mongoose.set('useCreateIndex', true)
+mongoose.set('useCreateIndex', true);
 
 //Carrega os Models
 const Product = require('./models/product');
